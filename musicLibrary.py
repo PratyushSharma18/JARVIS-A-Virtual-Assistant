21a9d434b3fec713ccb5de264dd1b1043df975af
Pratyush Sharma <pratyushvkp@gmail.com>
2025-03-09 00:06:20 +0530

    Final Commit

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "alone": "https://www.youtube.com/watch?v=1-xGerv5FOk",
+    "montero": "https://www.youtube.com/watch?v=vP1_Q1EfRgM",
+    "demons": "https://www.youtube.com/watch?v=MA0aCUxItYA",
+    "photograph": "https://www.youtube.com/watch?v=SlbfAYvA_gI"
+    # More could be added as per choices for directlinks
+}
